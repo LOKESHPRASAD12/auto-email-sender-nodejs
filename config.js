0520c9011e97b788cd543f5d49145211504e1bf0
LOKESHPRASAD12 <lokeshprasad125@gmail.com>
2023-05-24 20:21:38 +0530

    assignment

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports={
+    user:'lokeshprasad125@gmail.com',
+    clientId: '958377330570-ij6i2jk5p7me499dfqs8q7hvdhadqcqt.apps.googleusercontent.com',
+    clientSecret: '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',
+    refreshToken: '1//04hj6roL5H5VDCgYIARAAGAQSNwF-L9IrchcAbXsLW0bB3-OvRmThz-Pm_GtHH2_jDSVp-LHKxTukurm-lOAF66Y03_T7tTWam8k'
+}
